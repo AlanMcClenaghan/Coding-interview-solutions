81d38bf40a53e5adb97ce78a31a5cc867df32f43
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-24 14:10:25 +0000

    palindromes

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Most strightfrward solution
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+// Using the every() helper method
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1]
+//   });
+// }
 
 module.exports = palindrome;
